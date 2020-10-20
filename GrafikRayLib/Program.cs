f0b4f52451e25f70b39920ed833d67851642952a
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-10-20 10:28:54 +0200

    RayLib

--- GrafikRayLib/Program.cs
+++ GrafikRayLib/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Raylib_cs;
 
 namespace GrafikRayLib
 {
@@ -6,7 +7,43 @@ namespace GrafikRayLib
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Raylib.InitWindow(800, 600, "Hello World!");
+
+            Color myColor = new Color(0, 225, 128, 187);
+
+            float x = 0;
+            float y = 0;
+            //Så länge fönstret inte ska stängas; fortsätt while
+            while(!Raylib.WindowShouldClose())
+            {
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                {
+                    x += 0.1f;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {
+                    x -= 0.1f;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+                {
+                    y -= 0.1f;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                {
+                    y += 0.1f;
+                }
+                
+
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(myColor);
+
+                Raylib.DrawRectangle((int)x, (int)y, 20, 30, Color.RED);
+
+                Raylib.EndDrawing();
+            }
+
         }
     }
 }
